83b381209df240813ebf8935565dac7ec00b04ec
Anatoly Levitsky <levitsky@element-soft.com>
2025-08-24 15:29:10 +0300

    update dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.lewickiy.snake {
     requires javafx.controls;
     requires javafx.fxml;
-    requires lombok;
+    requires javafx.media;
+    requires static lombok;
+
+    requires junit;
+
     opens com.lewickiy.snake to javafx.fxml;
     exports com.lewickiy.snake;
 }
